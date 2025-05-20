b8315bf8804bd92c9bd6051692bec803cf85f539
Daniela-0819 <danicaslopera2005@gmail.com>
2025-05-20 11:15:16 -0500

    estilo

--- app/page.js
+++ app/page.js
@@ -1,38 +1,39 @@
 //al usar useState y useEffect, que solo funcionan en componentes cliente (en el navegador) debo poner esa linea.
-"use client"; 
+"use client";
 import { useEffect, useState } from "react";
 import React from 'react';
 import Tarea from "./components/Tarea";
+import styles from './style.module.css';
 
 export default function Page() {
 
-  const [tareas, setTareas] = useState([]); 
+  const [tareas, setTareas] = useState([]);
   const [descripcion, setDescripcion] = useState("");
   const [fecha, setFecha] = useState("");
-  const[filtro, setFiltro] = useState("");//este es para filtrar las tareas pendientes y las hechas
+  const [filtro, setFiltro] = useState("");//este es para filtrar las tareas pendientes y las hechas
 
   //use efect
-    useEffect(() =>{
-      const tareasGuardadas = localStorage.getItem("tareas");
-      if (tareasGuardadas){
-        setTareas(JSON.parse(tareasGuardadas)); //convierte el texto en un arreglo
-      }
-    }, []);
+  useEffect(() => {
+    const tareasGuardadas = localStorage.getItem("tareas");
+    if (tareasGuardadas) {
+      setTareas(JSON.parse(tareasGuardadas)); //convierte el texto en un arreglo
+    }
+  }, []);
 
-    //este es para convertir el arreglo en un texto con formato JSON.stringfy
-    useEffect(() => {
-      localStorage.setItem("tareas", JSON.stringify(tareas));
-    }, [tareas]); 
+  //este es para convertir el arreglo en un texto con formato JSON.stringfy
+  useEffect(() => {
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+  }, [tareas]);
 
-    useEffect(() =>{
-      // Aquí luego pondremos código para actualizar la lista filtrada
-    }, [filtro]);
+  useEffect(() => {
+    // Aquí luego pondremos código para actualizar la lista filtrada
+  }, [filtro]);
 
-    useEffect(() =>{
-      const pendientes = tareas.filter(t => !t.hecha).length;//aca la t es una variable temporal
-      console.log(`Tienes ${pendientes} tareas pendientes`);
+  useEffect(() => {
+    const pendientes = tareas.filter(t => !t.hecha).length;//aca la t es una variable temporal
+    console.log(`Tienes ${pendientes} tareas pendientes`);
 
-    }, [tareas]);
+  }, [tareas]);
 
   //funcion para poder agregar las tareas
   function agregarTarea() {
@@ -49,25 +50,31 @@ export default function Page() {
   }
 
   function calificarTarea() {
-    const hecha =  true;
+    const hecha = true;
 
   }
 
   //el return debe quedar dentro de la funcion, si no, no da
   return (
-    <div>
-      <input type="text" value={descripcion} onChange={(e) => setDescripcion(e.target.value)}/>
-      <input type="date" value={fecha} onChange={(e) => setFecha(e.target.value)}/>
-      <button onClick={agregarTarea}>Agregar Tarea</button>
-      <button onClick={calificarTarea}>Marcar como hecha</button>
-      <ul>
+    <div className={styles.contenedor}>
+      <div className={styles.form}>
+        <h1>Agregar nueva tarea</h1>
+        <input type="text" value={descripcion} onChange={(e) => setDescripcion(e.target.value)} /><br></br>
+        <input type="date" value={fecha} onChange={(e) => setFecha(e.target.value)} /><br></br>
+        <button onClick={agregarTarea}>Agregar Tarea</button><br></br>
+        <button onClick={calificarTarea}>Marcar como hecha</button>
+      </div>
+
+      <div className={styles.result}>
+        <h1>Tareas</h1>
+        <ul>
           {tareas.map(tarea => (
             <li key={tarea.id}>
-            {tarea.descripcion} - {tarea.fecha} - {tarea.hecha ? "Hecha" : "Pendiente"}
+              {tarea.descripcion} - {tarea.fecha} - {tarea.hecha ? "Hecha" : "Pendiente"}
             </li>
-          
           ))}
-      </ul>
+        </ul>
+      </div>
     </div>
   );
 }
